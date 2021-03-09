66541b9da4bbcc995ea39f155d41d7cf0f2e0c2f
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-09 12:14:42 +0530

    Update merge_sort1.go

--- merge_sort1.go
+++ merge_sort1.go
@@ -1,23 +1,26 @@
+//Mergesort implementation in Golang
 package main
 
 import (
 	"fmt"
 )
-
+//defining func for mergesort
 func MergeSort(numbers []int) []int {
 	if len(numbers) < 2 {
 		return numbers
 	}
 	mid := int(len(numbers) / 2)
+	//calling mergesort func recursively 
 	return merge(MergeSort(numbers[mid:]), MergeSort(numbers[:mid]))
 }
-
+//defining another func for merging
 func merge(a, b []int) []int {
 	size := len(a) + len(b)
 	i := 0
 	j := 0
 	res := make([]int, size)
 	for k := 0; k < size; k++ {
+		//switch case
 		switch true {
 		case i == len(a):
 			res[k] = b[j]
@@ -41,6 +44,7 @@ func merge(a, b []int) []int {
 }
 func main() {
 	a := []int{8, 5, 2, 7, 5, 12}
+	//calling function for mergesort
 	fmt.Println(MergeSort(a))
 
 }
