b123e222b10769b1ab14332358833325b8c594f3
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-09 12:07:23 +0530

    Update bubblesort1.go

--- bubblesort1.go
+++ bubblesort1.go
@@ -1,16 +1,20 @@
+//bubblesort implementation in Golang
 package main
 
 import "fmt"
-
+//defining func 
 func bubblesort(input []int) {
 	n := len(input)
+	//boolean type
 	swap := true
 	for swap {
 		swap = false
 		for i := 1; i < n; i++ {
 			if input[i-1] > input[i] {
 				fmt.Println("swapping")
+				//swapping the elements
 				input[i-1], input[i] = input[i], input[i-1]
+				//to repeat the process
 				swap = true
 			}
 		}
@@ -20,7 +24,7 @@ func bubblesort(input []int) {
 func main() {
 	var a []int = []int{4, 8, 23, 0, 5, 9, 3, 15}
 	b := []int{1, 53, 8, 6, 4, 0}
-
+       //calling func
 	bubblesort(a)
 	bubblesort(b)
 }
