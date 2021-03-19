b113f6e19f291f0152e97ffe0fd97a567094ca4e
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-19 11:37:39 +0530

    Update insertion-sort.go

--- insertion-sort.go
+++ insertion-sort.go
@@ -1,13 +1,14 @@
+//insertion sort in Golang
 package main
 
 import "fmt"
-
+//defining func for insertion sort
 func InsertionSort(numbers []int) []int {
 	for i := 0; i < len(numbers); i++ {
 		for j := 0; j < i+1; j++ {
-			//compare element present at index i with every element present
+			//comparing element present at index i with every element present
 			//  left of it place it in right place so that array on the
-			//left remains   sorted
+			//left remains sorted
 			if numbers[j] > numbers[i] {
 
 				intermediate := numbers[j]
@@ -15,7 +16,7 @@ func InsertionSort(numbers []int) []int {
 				numbers[i] = intermediate
 			}
 		}
-		//fmt.Println(numbers)
+		
 	}
 	return numbers
 }
